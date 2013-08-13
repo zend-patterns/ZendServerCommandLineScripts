fdbbe8ae425ee843777b385405e75c7e83e7e94e
Nick Maiorsky <nickma@gmail.com>
2013-08-13 10:10:20 -0700

    Mysql example simple install

---
+++ zs53mysql-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Import global conf 
+. $global_conf
+
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+set -e
+
+# Tested on CentOS
+if [ -x /usr/sbin/selinuxenabled ] && /usr/sbin/selinuxenabled; then
+    # SELinux can be disabled by setting "/usr/sbin/setenforce Permissive"
+    echo 'SELinux in enabled on this instance.  This service requires SELinux to be disabled to install successfully'
+    exit 1
+fi
+
+if [ "x$OS" != "x" ] && [ $OS = 'Ubuntu' ]; then
+    # Fix the linux-firmware package 
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get install -y linux-firmware < /dev/console > /dev/console 
+    # Install MySQL package 
+    apt-get install -y mysql-server
+else 
+    yum --nogpgcheck --noplugins -y install -x MySQL-server-community mysql-server
+fi
