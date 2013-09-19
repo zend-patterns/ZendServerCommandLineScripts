85ce989800b02308fada2aba4e9abdbd40d22394
Nick Maiorsky <nickma@gmail.com>
2013-09-18 20:02:18 -0700

    Global updated

--- globals.sh
+++ globals.sh
@@ -1,10 +1,8 @@
-#!/bin/bash -e
-
 zendadmin_ui_pass='zulutango99'
 zenddev_ui_pass='zalgreb00'
 zend_order_number='Zulu-IL999'
 zend_admin_email='nick@zend.com'
-zend_license_key='01127HI0801G212E00245E74A40E3C81'
+zend_license_key=''
 zend_self_name=`hostname`
 zend_self_addr=`ip a s dev eth0 | grep -oP 'inet\s+\K[^/]+'`
 zend_db_host='192.168.15.98'
--- zs-setupjoin.sh
+++ zs-setupjoin.sh
@@ -1,29 +1,6 @@
 #!/bin/bash -ex
 
-# Zend Web Administartion password for user Admin
-zendadmin_ui_pass='zulutango99'
-# Zend Web Administartion password for user Developer
-zenddev_ui_pass='zalgreb00'
-# Zend Server Purchise order number.
-zend_order_number='Zulu-IL999'
-# Zend Server License Key.
-zend_license_key=''
-# Zend Monitoring email.
-zend_admin_email='nick@zend.com'
-# Node hostname or nick name if specified.
-zend_self_name=`hostname`
-# Node IP address eth0
-zend_self_addr=`ip a s dev eth0 | grep -oP 'inet\s+\K[^/]+'`
-# Zend Cluster MySQL DB hostname or address.
-zend_db_host=''
-# Zend Cluster MySQL DB user name with admin priveleges. 
-zend_db_user='root'
-# Zend Cluster MySQL DB password.
-zend_db_password='support'
-# Zend Cluster Database name.
-zend_db_name='testdb001'
-# PHP version branch used by on the server. Same version of PHP on all the nodes is required. Use 5.3 or 5.4. 
-zend_php_ver='5.4'
+. ./globals.sh
 
 
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
